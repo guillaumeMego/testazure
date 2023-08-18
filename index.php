64d85e9752576d9eb85f900791e38947b866ddce
Guillaume Ganne <guillaumeganne.dev@gmail.com>
2023-08-18 10:49:56 +0200

    bddauth

--- index.php
+++ index.php
@@ -3,18 +3,12 @@
 echo "Hello World!";
 
 
-try {
-    $conn = new PDO("sqlsrv:server = tcp:servertestguillaume.database.windows.net,1433; Database = basetest", "CloudSA581e4b7f", "{your_password_here}");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-}
-
-// SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "CloudSA581e4b7f", "pwd" => "{your_password_here}", "Database" => "basetest", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:servertestguillaume.database.windows.net,1433";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
-?>
+$connectionOptions = array(
+    "Database" => "basetest",
+    "Authentication" => "ActiveDirectoryInteractive"
+);
+
+$conn = new PDO("sqlsrv:$serverName", $connectionOptions);
+
 
